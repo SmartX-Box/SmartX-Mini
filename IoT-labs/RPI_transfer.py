a5bcb48db4aa27e17ffa5b478d46a361c3a449fd
seunghyungLee <lshyeung@smartx.kr>
2019-03-05 15:56:18 +0900

    Update RPI_transfer.py

--- IoT-labs/RPI_transfer.py
+++ IoT-labs/RPI_transfer.py
@@ -3,7 +3,7 @@ import urllib2
 from time import sleep
 
 def transfer():
-        f=open("/root/data.txt",'r')
+        f=open("./data.txt",'r')
         line = f.readline()
         words = line.split(" ")
         print(words)
